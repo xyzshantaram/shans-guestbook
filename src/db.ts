e045b621d309964dec59c5bda8359ad8023ad069
Siddharth Singh <me@shantaram.xyz>
2023-08-19 23:00:42 +0530

    first draft of db queries

--- src/db.ts
+++ src/db.ts
@@ -7,5 +7,21 @@ const INIT_STATEMENTS = [
     await Deno.readTextFile('./messages-migration.sql'),
 ]
 
-export const db = new Database('./guestbook.db');
+const db = new Database('./guestbook.db');
 INIT_STATEMENTS.forEach(stmt => db.exec(stmt));
+
+const getMessageQuery = db.prepare("SELECT rowid, name, message, added_on FROM messages LIMIT :limit OFFSET :offset");
+
+export const getMessages = (page?: number) => {
+    const limit = 50;
+    let offset = 0;
+
+    if (page) offset = limit * page;
+    return getMessageQuery.all({ limit, offset });
+}
+
+const anonMessageQuery = db.prepare("INSERT INTO messages (message) values (:message)");
+const namedMessageQuery = db.prepare("INSERT INTO messages (name, message) values (:name, :message)");
+export const addMessage = (message: string, name?: string) => {
+    (name?.trim() ? namedMessageQuery : anonMessageQuery).run({ name, message });
+}
