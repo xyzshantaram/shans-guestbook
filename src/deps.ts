b2fb6a58a27f9fd15aa3e84dd238aba52eb6590e
Siddharth Singh <me@shantaram.xyz>
2023-12-31 15:06:16 +0530

    update deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,4 @@
 import * as RateLimiterFlexible from "https://esm.sh/rate-limiter-flexible@2.4.2";
-import { Database } from "https://deno.land/x/sqlite3@0.9.1/mod.ts";
 import nhttp, { RequestEvent, NextFunction, HttpResponse } from "https://deno.land/x/nhttp@1.2.11/mod.ts";
 import serveStatic from "https://deno.land/x/nhttp@1.2.11/lib/serve-static.ts";
 import { Liquid, Template } from "https://esm.sh/liquidjs@10.8.4";
@@ -8,7 +7,7 @@ import * as fs from "https://deno.land/std@0.184.0/fs/mod.ts";
 import { DB } from "https://deno.land/x/sqlite@v3.8/mod.ts";
 
 export {
-    nhttp, serveStatic, RequestEvent, HttpResponse, Liquid, Database, fs, RateLimiterFlexible, DB
+    nhttp, serveStatic, RequestEvent, HttpResponse, Liquid, fs, RateLimiterFlexible, DB
 }
 
 export type {
